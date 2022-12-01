4aabe4c6023cf799312d893817b2fa8cec6b2abb
vishal kumar <vishal702355@gmail.com>
2022-12-01 21:23:52 +0530

    ur

--- src/Footer.js
+++ src/Footer.js
@@ -1,31 +1,51 @@
-import './Footer.css';
-import React from 'react';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Dropdown from 'react-bootstrap/Dropdown';
-import { Link } from 'react-router-dom';
- 
+import "./Footer.css";
+import React from "react";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
+import { Link } from "react-router-dom";
+
 const Footer = () => (
   <footer className="footer">
-      <div className="link">
-         <Link to ='/'> <a className="home">HOME</a></Link>
-            
-          <DropdownButton id="dropdown-basic-button" title="GAMES" >
-        
-          <ul>
- <li><Link to='/cardmemory'><Dropdown.Item href="#/action-1">Memory Game</Dropdown.Item></Link></li> 
-  <li><Link to='/tictactoe'><Dropdown.Item href="#/action-2">Tic-Tac-Toe</Dropdown.Item></Link></li>
-  <li><Link to='/rockpaper'><Dropdown.Item href="#/action-3">Rock-Paper-Scissor</Dropdown.Item></Link></li>
-  <li><Link to='/shapes'><Dropdown.Item href="#/action-4">Shapes Game</Dropdown.Item></Link></li>
-  </ul>
-</DropdownButton>
+    <div className="link">
+      <Link to="/">
+        {" "}
+        <a className="home">HOME</a>
+      </Link>
 
-      </div>
-    <div className="rights">
-        <a className="insta" href="App /"><i class="fab fa-instagram"></i></a>
-        <p className="all">@Shailja Bhaskar @Diya Banerjee, ALL RIGHTS RESERVED.</p>
+      <DropdownButton id="dropdown-basic-button" title="GAMES">
+        <ul>
+          <li>
+            <Link to="/cardmemory">
+              <Dropdown.Item href="#/action-1">Memory Game</Dropdown.Item>
+            </Link>
+          </li>
+          <li>
+            <Link to="/tictactoe">
+              <Dropdown.Item href="#/action-2">Tic-Tac-Toe</Dropdown.Item>
+            </Link>
+          </li>
+          <li>
+            <Link to="/rockpaper">
+              <Dropdown.Item href="#/action-3">
+                Rock-Paper-Scissor
+              </Dropdown.Item>
+            </Link>
+          </li>
+          <li>
+            <Link to="/shapes">
+              <Dropdown.Item href="#/action-4">Shapes Game</Dropdown.Item>
+            </Link>
+          </li>
+        </ul>
+      </DropdownButton>
     </div>
-  
+    <div className="rights">
+      <a className="insta" href="App /">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <p className="all">Copyright@2022</p>
+    </div>
   </footer>
 );
-  
+
 export default Footer;
